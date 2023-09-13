28cc828d82f89eb938712ffde1fd3092e3fd38c3
devtatsuki <devtatsuki@gmail.com>
2023-09-13 10:55:49 -0300

    lesson: 2 avançando o back-end da aplicação

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,11 +1,21 @@
+import { fastifyCors } from '@fastify/cors';
 import { fastify } from 'fastify';
+import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAICompletionRoute } from './routes/generate-ai-completion';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
 
-const app = fastify()
+const app = fastify();
 
-app.get('/', () => {
-  return 'Hello World';
+app.register(fastifyCors, {
+  origin: '*'
 })
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
 
 app.listen({
   port: 3333,
-}).then(() => console.log('🚀 HTTP Server Running!'))
+}).then(() => console.log('🚀 HTTP Server Running!'));
